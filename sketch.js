6f7335ee582a5d38089e362f39d8a4cc829589c2
daniel45roblox <161093048+daniel45roblox@users.noreply.github.com>
2024-04-05 19:56:38 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,19 +3,29 @@ var vida = 1;
 var balas = 0;
 var game_status = 0;
 var enemigos_lista = ["geometry_rob", "calcuROB"];
+var corazones = 3;
 function preload() {
     bob_caminando = loadAnimation("./sprites/sprite_00.png", "./sprites/sprite_01.png", "./sprites/sprite_02.png", "./sprites/sprite_03.png", "./sprites/sprite_04.png", "./sprites/sprite_05.png", "./sprites/sprite_06.png", "./sprites/sprite_07.png", "./sprites/sprite_08.png", "./sprites/sprite_09.png", "./sprites/sprite_10.png", "./sprites/sprite_11.png", "./sprites/sprite_12.png", "./sprites/sprite_13.png", "./sprites/sprite_14.png", "./sprites/sprite_15.png", "./sprites/sprite_16.png", "./sprites/sprite_17.png", "./sprites/sprite_18.png", "./sprites/sprite_19.png");
     bob_quieto = loadAnimation("./sprites/sprite_00.png", "./sprites/sprite_01.png", "./sprites/sprite_02.png", "./sprites/sprite_03.png", "./sprites/sprite_04.png", "./sprites/sprite_05.png");
     bob_corre = loadAnimation("./sprites/sprite_18.png", "./sprites/sprite_19.png")
     bob_salta = loadAnimation("./sprites/BOB saltando 0.png", "./sprites/BOB saltando 1.png", "sprites/BOB saltando 2.png", "./sprites/BOB saltando 3.png", "./sprites/BOB saltando 4.png")
-    bob_atacando = loadAnimation("./sprites/BOB ATACANDO00.png","./sprites/BOB ATACANDO01.png","./sprites/BOB ATACANDO02.png","./sprites/BOB ATACANDO03.png","./sprites/BOB ATACANDO04.png","./sprites/BOB ATACANDO05.png","./sprites/BOB ATACANDO05.png","./sprites/BOB ATACANDO06.png","./sprites/BOB ATACANDO06.png","./sprites/BOB ATACANDO06.png","./sprites/BOB ATACANDO06.png","./sprites/BOB ATACANDO07.png","./sprites/BOB ATACANDO08.png","./sprites/BOB ATACANDO08.png","./sprites/BOB ATACANDO08.png","./sprites/BOB ATACANDO09.png","./sprites/BOB ATACANDO10.png","./sprites/BOB ATACANDO11.png","./sprites/BOB ATACANDO12.png");
+    bob_atacando = loadAnimation("./sprites/BOB ATACANDO00.png", "./sprites/BOB ATACANDO01.png", "./sprites/BOB ATACANDO02.png", "./sprites/BOB ATACANDO03.png", "./sprites/BOB ATACANDO04.png", "./sprites/BOB ATACANDO05.png", "./sprites/BOB ATACANDO05.png", "./sprites/BOB ATACANDO06.png", "./sprites/BOB ATACANDO06.png", "./sprites/BOB ATACANDO06.png", "./sprites/BOB ATACANDO06.png", "./sprites/BOB ATACANDO07.png", "./sprites/BOB ATACANDO08.png", "./sprites/BOB ATACANDO08.png", "./sprites/BOB ATACANDO08.png", "./sprites/BOB ATACANDO09.png", "./sprites/BOB ATACANDO10.png", "./sprites/BOB ATACANDO11.png", "./sprites/BOB ATACANDO12.png");
+    bob_gameover = loadAnimation("./sprites/game oVer00.png","./sprites/game oVer01.png","./sprites/game oVer02.png","./sprites/game oVer03.png","./sprites/game oVer04.png","./sprites/game oVer05.png","./sprites/game oVer06.png","./sprites/game oVer07.png","./sprites/game oVer08.png","./sprites/game oVer09.png","./sprites/game oVer10.png","./sprites/game oVer11.png");
     fondo = loadImage("backgroundforest.jpg");
+    arbustosImg = loadImage("./sprites/backgroundarbustos.png");
     bosstierra_caminando = loadAnimation("./sprites/boss de tierra0.png", "./sprites/boss de tierra1.png")
     bosstierra_paracaidas = loadAnimation("./sprites/boss de tierra 3.00.png", "./sprites/boss de tierra 3.01.png", "./sprites/boss de tierra 3.02.png", "./sprites/boss de tierra 3.03.png", "./sprites/boss de tierra 3.04.png", "./sprites/boss de tierra 3.05.png", "./sprites/boss de tierra 3.06.png")
     geometry_rob_img = loadAnimation("./sprites/geometry_rob.png");
+    avion_img = loadAnimation("./sprites/avion0.png","./sprites/avion1.png","./sprites/avion2.png","./sprites/avion3.png");
     rob_morir_img = loadAnimation("./sprites/muerte de un enemigo0.png", "./sprites/muerte de un enemigo1.png");
     calcuROB_IMG = loadAnimation("./sprites/calcuROB0.png", "./sprites/calcuROB1.png");
-    islas_img = loadAnimation("./sprites/isla-1.png","./sprites/isla-1copia.png")
+    islas_img = loadAnimation("./sprites/isla-1.png", "./sprites/isla-1copia.png")
+    corazon_img = loadAnimation("./sprites/corazon0.png", "./sprites/corazon1.png");
+    islas_img = loadAnimation("./sprites/isla-1.png", "./sprites/isla-1copia.png");
+    btnStartImg = loadImage("./sprites/btnStart.png");
+    btnSkinsImg = loadImage("./sprites/btnSkins.png");
+    btnSponsorImg = loadImage("./sprites/btnSponsor.png");
+
 }
 function setup() {
     createCanvas(windowWidth, windowHeight)
@@ -28,28 +38,48 @@ function setup() {
     bordes = createEdgeSprites();
     bob = createSprite(400, height * 0.80);
     bob.saltando = false;
+    bob.atacando = false;
     bob.addAnimation("caminar", bob_caminando);
     bob.addAnimation("correr", bob_corre);
     bob.addAnimation("quieto", bob_quieto);
     bob.addAnimation("saltar", bob_salta);
     bob.addAnimation("atacar", bob_atacando);
+    bob.addAnimation("gameover", bob_gameover);
+    arbustosImg.resize(width, height);
+    escena1_a = createSprite(width * 0.5, height / 2, width, height);
+    escena2_a = createSprite(width * 1.5, height / 2, width, height);
+    escena1_a.addImage(arbustosImg);
+    escena2_a.addImage(arbustosImg);
+    escena2_a.mirrorX(-1);
     suelo = createSprite(width * 0.5, height * 0.9, width, 10);
     suelo.visible = false;
-    bob.debug = false;
+    bob.debug = 0;
     crearMenuPrincipal();
     boss_tierra_grupo = new Group()
     enemigos_grupo = new Group()
-    /*for (let num_boss = 0; num_boss <= 4; num_boss++) {
-        crearBoss("tierra")
-    }*/
+    vidas = new Group()
+    for (let i = 0; i < corazones; i++) {
+        crearCorazon(i)
+    }
+    islas_grupo = new Group()
+    for (let num_islas = 0; num_islas <= 4; num_islas++) {
+        plataforma = createSprite(random(0, width*2), random(height*0.5, height*0.75))
+        plataforma.addAnimation("isla", islas_img)
+        plataforma.debug = 0
+        plataforma.depth = 6
+        plataforma.setCollider("rectangle", 0,30,100,30)
+        islas_grupo.add(plataforma)
+    }
 }
 function draw() {
     drawSprites()
+    createEnemies()
     bob.collide(suelo, dejardesaltar);
     bob.collide(bordes);
-    bob.overlap(enemigos_grupo,destruir)
+    bob.overlap(enemigos_grupo, destruir);
+    bob.collide(islas_grupo, dejardesaltar);
+    bob.overlap(btnStart, start);
     bob.velocityY = 5;
-    createEnemies()
     if (keyDown(LEFT_ARROW)) {
         bob.x = bob.x - 5;
         if (!bob.saltando) {
@@ -61,8 +91,13 @@ function draw() {
         if (game_status == 1 && bob.x > width * 0.55) {
             moverEscena(escena1)
             moverEscena(escena2)
+            moverEscena(escena1_a)
+            moverEscena(escena2_a)
+            //moverEscena(islas_grupo)
+            enemigos_grupo.setVelocityXEach(random(-10, -15));
         } else {
             bob.x = bob.x + 5.5;
+            enemigos_grupo.setVelocityXEach(random(-5, -10));
         }
         if (!bob.saltando) {
             bob.changeAnimation("correr");
@@ -74,21 +109,22 @@ function draw() {
         bob.velocityY = -100
         bob.saltando = true;
     }
-    if (bob.getAnimationLabel() == "atacar" && bob.animation.getFrame() == bob.animation.getLastFrame()) {
-        bob.changeAnimation("quieto")
-        //bob.animation.frameDelay = 4
-        //bob.animation.looping = true
-    }
-    if (!keyDown(LEFT_ARROW) && !keyDown(RIGHT_ARROW) && !bob.saltando) {
+    if (!keyDown(LEFT_ARROW) && !keyDown(RIGHT_ARROW) && !bob.saltando && !bob.atacando) {
         bob.changeAnimation("quieto");
         escena1.velocityX = 0;
         escena2.velocityX = 0;
+        escena1_a.velocityX = 0;
+        escena2_a.velocityX = 0;
     }
-    if(keyDown(90) || keyDown("z")){
+    /*if (keyWentDown("z") && !bob.atacando) {
+        bob.atacando = true
         bob.changeAnimation("atacar");
         bob.animation.changeFrame(0)
-        //bob.animation.frameDelay = 1
-        //bob.animation.looping = false
+    }*/
+    if (bob.getAnimationLabel() == "atacar" && bob.animation.getFrame() == bob.animation.getLastFrame()) {
+        bob.atacando = false
+        bob.saltando = false;
+
     }
 }
 function moverEscena(imagen) {
@@ -96,33 +132,47 @@ function moverEscena(imagen) {
     if (imagen.x <= -width * 0.5) {
         imagen.x = width * 1.5;
     }
+    if(imagen[0]){
+        islas_grupo.forEach(element => {
+            element.x-=5
+            if (element.x <= -width * 0.5) {
+                element.x = width * 1.5;
+            }
+        });
+    }
 }
 function perderVida() {
-
+corazones=corazones-0.5
+    if(corazones%1==0){
+        vidas[corazones].destroy()
+    }else{
+        vidas[corazones-0.5].animation.changeFrame(1)
+    }
 }
 function dejardesaltar(bob, suelo) {
+    if (!bob.atacando) {
+        bob.changeAnimation("quieto");
+    }
     bob.saltando = false;
-    bob.changeAnimation("quieto");
 }
 function crearMenuPrincipal() {
     menu = createDiv();
     menu.center()
     menu.id("menu");
-    botones = createDiv();
-    botones.id("botones");
     title = createImg("./sprites/title.png");
-    btnStart = createImg("./sprites/btnStart.png");
-    btnStart.parent("botones");
-    btnStart.mouseClicked(start);
-    btnSkins = createImg("./sprites/btnSkins.png");
-    btnSkins.parent("botones");
-    btnSponsor = createImg("./sprites/btnSponsor.png");
-    btnSponsor.parent("botones");
+    btnStart = createSprite(width * 0.3, height * 0.55);
+    btnStart.addImage(btnStartImg)
+    btnSkins = createSprite(width * 0.5, height * 0.55);
+    btnSkins.addImage(btnSkinsImg)
+    btnSponsor = createSprite(width * 0.7, height * 0.55);
+    btnSponsor.addImage(btnSponsorImg)
     title.parent("menu");
-    botones.parent("menu");
 }
 function start() {
     menu.hide();
+    btnStart.destroy()
+    btnSponsor.destroy()
+    btnSkins.destroy()
     game_status = 1;
 }
 function crearBoss(tipo) {
@@ -140,6 +190,12 @@ function crearBoss(tipo) {
             break;
     }
 }
+function crearCorazon(i) {
+    corazon = createSprite(width * 0.4 + (i * 50), 100)
+    corazon.addAnimation("corazon", corazon_img)
+    corazon.pause()
+    vidas.add(corazon);
+}
 function aterrizar_boss() {
     boss_tierra_grupo.forEach(element => {
         if (element.cayendo == true && element.isTouching(suelo)) {
@@ -149,10 +205,11 @@ function aterrizar_boss() {
     });
 }
 function createEnemies() {
-    if (game_status ===1 && frameCount % 55 == 0) {
+    if (game_status === 1 && frameCount % 55 == 0) {
         enemigo = createSprite(random(width * 0.85, width * 2.5), height * 0.85, 50, 50)
+        enemigo.depth = 7
         tipo = random(enemigos_lista)
-        enemigo.scale=2
+        enemigo.scale = 2
         switch (tipo) {
             case "geometry_rob":
                 enemigo.addAnimation("caminar", geometry_rob_img)
@@ -172,8 +229,11 @@ function createEnemies() {
         enemigos_grupo.add(enemigo)
     }
 }
-function destruir(bob,enemigo){
-    if (bob.getAnimationLabel() != "quieto"){
+function destruir(bob, enemigo) {
+    if (bob.getAnimationLabel() != "quieto") {
+        enemigo.destroy()
+    }else{
+        perderVida()
         enemigo.destroy()
     }
 }
